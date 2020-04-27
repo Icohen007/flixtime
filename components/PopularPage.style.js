a12e0300845c502f2818789cfdee9e7fd6ea3f3a
Icohen007 <37484485+Icohen007@users.noreply.github.com>
2020-04-27 23:26:44 +0300

    change pagination

--- components/PopularPage.style.js
+++ components/PopularPage.style.js
@@ -50,7 +50,7 @@ export const GridContainer = styled.div`
    max-width: 1150px;
    width: 100%;
    margin: auto;
-   padding: 10rem 10rem 50rem 10rem;
+   padding: 10rem 10rem 70rem 10rem;
    justify-content: center;
    text-align: center;
 `;
